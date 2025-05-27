1a3d12e0ee878c6bc23fffce7b8507a9066292ae
Piccoli Augusto <piccoli_44@hotmail.com>
2025-05-27 14:32:17 -0300

    Landing page y creación de estilos

--- PilatesGravity/urls.py
+++ PilatesGravity/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import home, crear_cliente, crear_clase, crear_turno, buscar_turno
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('crear_clase/', crear_clase, name='crear_clase'),
     path('crear_turno/', crear_turno, name='crear_turno'),
     path('buscar_turno/', buscar_turno, name='buscar_turno'),
+    path('politica-de-privacidad/', TemplateView.as_view(template_name='PilatesGravity/politica_privacidad.html'), name='politica_privacidad'),
 ]
